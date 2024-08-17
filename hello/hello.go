e4edbfc80921d0b036fdfc3bb822e81a1b4a4c58
fanaticf <iamfanaticf@gmail.com>
2024-08-17 23:56:01 +0530

    hello.go refactor

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+const englistHelloPrefix = "Hello"
+
 func Hello(name string) string {
-  return "Hello, " + name
+  return englistHelloPrefix + ", " + name
 }
 
 func main() {
