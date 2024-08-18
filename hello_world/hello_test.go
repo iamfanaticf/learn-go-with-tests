a007a5fae16004d1df0b3a680abe565fa2966826
fanaticf <iamfanaticf@gmail.com>
2024-08-18 15:13:30 +0530

    added a directory

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -5,17 +5,29 @@ import "testing"
 func TestHello(t *testing.T) {
 
   t.Run("saying hello to people", func(t *testing.T) {
-    got := Hello("Chris")
+    got := Hello("Chris", "")
     want := "Hello, Chris"
     assertCorrectMessage(t, got, want)
   })
 
   t.Run("empty string defaults to 'world' ", func(t *testing.T) {
-    got := Hello("")
-    want := "Hello, world"
+    got := Hello("", "")
+    want := "Hello, World"
     assertCorrectMessage(t, got, want)
   })
 
+  t.Run("in Spanish", func(t *testing.T) {
+    got := Hello("Elodie", "Spanish")
+    want := "Hola, Elodie"
+    assertCorrectMessage(t, got, want)
+  })
+
+  t.Run("in French", func(t *testing.T) {
+    got := Hello("Fransis", "French")
+    want := "Bonjour, Fransis"
+    assertCorrectMessage(t, got, want)
+  })
+  
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
